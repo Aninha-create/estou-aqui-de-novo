9172e21d7b17615bf3898a6b80577920a3490404
Ana Rachel Rodrigues da Costa <anasophiaerajh@gmail.com>
2025-02-06 18:19:56 -0300

    Add files via upload
    Criei um arquivo no javascript.

---
+++ 01desafio.js
@@ -0,0 +1,14 @@
+alert ('Continue tentando! você conseguirá!');
+let estouTentando = 10;
+console.log ( estouTentando);
+let tentativa;
+tentativa=prompt ('Quantas tentativas você fez até agora?');
+if (tentativa == estouTentando){
+    alert('Parabéns!');
+} else {
+    ( estouTentando >10);
+    alert('Você precisa estudar mais!')
+}
+
+
+
